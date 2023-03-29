98a24fd0d9ae60a1d2b9da1cc0d7810dec93f6f1
Antonio <avps2@aluno.ifal.edu.br>
2023-03-29 20:27:10 -0300

    att de calculo

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -18,27 +18,38 @@ function insert(num)
 
 }
 
-// testeal()
+
+// teste numero par
+
+function numeroPar() {
+    var calculo = document.getElementById('calculo').innerHTML;
+
+    if (calculo % 2 == 0) {
+        
+    }
+    document.getElementById('calculo').innerHTML = eval(Math.sqrt(calculo));
+}
+// fim teste par
+// teste fatorial
+// funcionando
+// console.log('antes da função')
+function fatorial() {
+    var calculo = document.getElementById('calculo').innerHTML;
+    let somatorio = 1;
+    for (let i= 1; i <= calculo; i++)
+    {
+        somatorio *=i;
+    }  
+    // console.log(somatorio);
+document.getElementById('calculo').innerHTML = somatorio;
+}
+        // console.log('depois da função')
 
 // funcionando
 
 function raiz() {
     var calculo = document.getElementById('calculo').innerHTML;
     document.getElementById('calculo').innerHTML = eval(Math.sqrt(calculo));
-    
-    
-    //  var numeroRaiz = document.getElementById('calculo').innerHTML;
-    // document.getElementById('calculo').innerHTML = Math.sqrt(numeroRaiz);
-    
-    // var calculo = document.getElementById('calculo').innerHTML;
-    // if(calculo)
-    // {
-    //     document.getElementById('calculo').innerHTML = eval(calculo);
-    // }
-    // else
-    // {
-    //     document.getElementById('calculo').innerHTML = "Null..."
-    // }
 }
 
 function clean()
